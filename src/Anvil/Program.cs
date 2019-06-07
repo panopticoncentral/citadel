8dfb7e0deba8a1f95ae09a8bebfd0dd3dfd30558
Paul Vick <paulv@microsoft.com>
2019-06-06 22:10:28 -0700

    More base

---
+++ src/Anvil/Program.cs
@@ -0,0 +1,13 @@
+﻿using SdlSharp;
+
+namespace Anvil
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Sdl.Initialize(Subsystems.Video);
+            Sdl.Quit();
+        }
+    }
+}
